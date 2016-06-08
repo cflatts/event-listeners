2bb660df9a3dc8d6816e00fe395bfbd924b6bf97
Caitlin Flattery <caitlinflattery@Caitlins-MacBook-Air.local>
2016-06-08 15:12:28 -0500

    event part 1

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 // GO!
 
+//PART 1
+var disappearButtonNode = document.querySelector('#button1')
+var barNode = document.querySelector('.nav-menu')
+
+var disappearBar = function() {
+    barNode.style.opacity = 0
+}
+
+disappearButtonNode.addEventListener('click', disappearBar)
