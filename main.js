1254a29f9d3ba22f2cb8d939fd2763444bf9d70b
Caitlin Flattery <caitlinflattery@Caitlins-Air.attlocal.net>
2016-06-09 07:28:01 -0500

    event part 2

--- main.js
+++ main.js
@@ -9,3 +9,37 @@ var disappearBar = function() {
 }
 
 disappearButtonNode.addEventListener('click', disappearBar)
+
+//PART 2
+
+var nameListNode = document.querySelector('.guest-list')
+var inputNode = document.querySelector('.answer-box input')
+
+var addName = function(clickEvent) {
+    if(clickEvent.keyCode === 13) {
+        var guestName = clickEvent.target.value
+        var liNode = document.createElement('li')
+        liNode.textContent = guestName
+        nameListNode.appendChild(liNode)
+        inputNode.value = ''
+    }
+}
+
+inputNode.addEventListener('keydown', addName)
+
+//PART 3
+
+var nameListNode = document.querySelector('.guest-list')
+var inputNode = document.querySelector('.answer-box input')
+
+var addName = function(clickEvent) {
+    if(clickEvent.keyCode === 13) {
+        var guestName = clickEvent.target.value
+        var liNode = document.createElement('li')
+        var removeNode = document.createElement('button')
+        liNode.textContent = guestName
+        nameListNode.appendChild(liNode)
+        nameListNode.appendChild(liNode)
+        inputNode.value = ''
+    }
+}
