54fa601bcab4be8c8ebe74928aadab6d0f61f545
Caitlin Flattery <caitlinflattery@gmail.com>
2016-10-12 17:41:54 -0500

    bar can toggle

--- main.js
+++ main.js
@@ -4,16 +4,27 @@
 var disappearButtonNode = document.querySelector('#button1')
 var barNode = document.querySelector('.nav-menu')
 
+var barState = {
+    hidden: false
+}
+
 var disappearBar = function() {
-    barNode.style.opacity = 0
+    if(barState.hidden) {
+        barNode.style.opacity = 1
+        barState.hidden = false
+    }
+    else {
+        barNode.style.opacity = 0
+        barState.hidden = true
+    }
 }
 
 disappearButtonNode.addEventListener('click', disappearBar)
 
 //PART 2
 
-var nameListNode = document.querySelector('.guest-list')
-var inputNode = document.querySelector('.answer-box input')
+var nameListNode = document.querySelector('#add-guest .guest-list')
+var inputNode = document.querySelector('#add-guest .answer-box input')
 
 var addName = function(clickEvent) {
     if(clickEvent.keyCode === 13) {
@@ -29,14 +40,13 @@ inputNode.addEventListener('keydown', addName)
 
 //PART 3
 
-var nameListNode = document.querySelector('.guest-list')
-var inputNode = document.querySelector('.answer-box input')
+var nameListNode = document.querySelector('#add-guest-bonus .guest-list')
+var inputNode = document.querySelector('#add-guest-bonus .answer-box input')
 
 var addName = function(clickEvent) {
     if(clickEvent.keyCode === 13) {
         var guestName = clickEvent.target.value
         var liNode = document.createElement('li')
-        var removeNode = document.createElement('button')
         liNode.textContent = guestName
         nameListNode.appendChild(liNode)
         nameListNode.appendChild(liNode)
